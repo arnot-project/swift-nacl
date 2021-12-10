a9c9b2cd4a7db7898f7b9abfd4cc508b411faf5a
greg <grzesiek.leszek@gmail.com>
2021-12-10 20:59:33 +0000

    refactor local variable

--- Sources/NaCl/NaCl.swift
+++ Sources/NaCl/NaCl.swift
@@ -5,19 +5,20 @@ public struct NaCl {
     public init() {}
     
     public func toPublic(_ input: [UInt8]) -> [UInt8] {
-        let pk = UnsafeMutablePointer<UInt8>.allocate(capacity: 32)
-        let sk = UnsafeMutablePointer<UInt8>.allocate(capacity: 32)
-        var pkCopy: [UInt8] = Array(repeating: 0, count: 32)
+        let capacity = 32
+        let pk = UnsafeMutablePointer<UInt8>.allocate(capacity: capacity)
+        let sk = UnsafeMutablePointer<UInt8>.allocate(capacity: capacity)
+        var pkCopy: [UInt8] = Array(repeating: 0, count: capacity)
 
-        for i in 0..<32 {
+        for i in 0..<capacity {
             (pk + i).initialize(to: 0)
             (sk + i).initialize(to: input[i])
         }
 
         modified_crypto_sign_publickey(pk, sk)
 
-        for index in 0..<pkCopy.count {
-            pkCopy[index] = (pk + index).pointee
+        for i in 0..<capacity {
+            pkCopy[i] = (pk + i).pointee
         }
 
         pk.deallocate()
